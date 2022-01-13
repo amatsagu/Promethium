d8e1a66020dc850e0093c8b696fbe0980b15059d
Amatsagu <81469656+Amatsagu@users.noreply.github.com>
2022-01-14 00:34:24 +0100

    Update helpers.ts

--- helpers.ts
+++ helpers.ts
@@ -1,6 +1,6 @@
 import { ResponseHelperOptions } from "./src/types.d.ts";
-import { render } from "https://x.lcas.dev/preact@10.5.12/ssr.js"; // JSX Support
-import type { VNode } from "https://x.lcas.dev/preact@10.5.12/mod.d.ts";
+// import { render } from "https://x.lcas.dev/preact@10.5.12/ssr.js"; // JSX Support
+// import type { VNode } from "https://x.lcas.dev/preact@10.5.12/mod.d.ts";
 
 export function $json(data: Record<string, unknown>, options?: ResponseHelperOptions) {
     return new Response(JSON.stringify(data), {
@@ -11,10 +11,12 @@ export function $json(data: Record<string, unknown>, options?: ResponseHelperOpt
 }
 
 /** We're using **"PREACT"** *(https://preactjs.com/)* to render your jsx code as an alternative to much slower original. */
-export function $jsx(data: VNode, options?: ResponseHelperOptions) {
-    return new Response(render(data), {
-        headers: (options?.headers !== undefined ? Object.assign(options!.headers, { "Content-Type": "text/html; charset=utf-8" }) : { "Content-Type": "text/html; charset=utf-8" }) as HeadersInit,
-        status: options?.statusCode ?? 200,
-        statusText: options?.statusText ?? "OK"
-    });
-}
+// export function $jsx(data: VNode, options?: ResponseHelperOptions) {
+//     return new Response(render(data), {
+//         headers: (options?.headers !== undefined ? Object.assign(options!.headers, { "Content-Type": "text/html; charset=utf-8" }) : { "Content-Type": "text/html; charset=utf-8" }) as HeadersInit,
+//         status: options?.statusCode ?? 200,
+//         statusText: options?.statusText ?? "OK"
+//     });
+// }
+
+// DISABLED JSX SUPPORT FOR NOW CAUSE LATEST RELEASE OF DENO DOESN'T WANT TO WORK WITH IT!
