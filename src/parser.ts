f56fecad8392677e8a58cd829ec17b221bdcbdaf
Amatsagu <81469656+Amatsagu@users.noreply.github.com>
2022-01-15 13:19:29 +0100

    Update parser.ts

--- src/parser.ts
+++ src/parser.ts
@@ -38,12 +38,3 @@ export function parse(route: Pattern, loose?: boolean): ParseResult {
         pattern: new RegExp("^" + pattern + (loose ? "(?=$|/)" : "/?$"), "i")
     };
 }
-
-export function parseBigInts(key: string, value: unknown) {
-  if (typeof value === "bigint") {
-    if (value > 9007199254740991) return value.toString();
-    // @ts-ignore BigInt has support for that!
-    return parseInt(value);
-  }
-  return value
-}
