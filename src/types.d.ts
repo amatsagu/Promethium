487a0a7c7bbc7bd697803070002b5fadbb898c11
Amatsagu <81469656+Amatsagu@users.noreply.github.com>
2022-03-01 19:02:15 +0100

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,7 +2,7 @@ export type RouteResponse = Response | void | Promise<Response | void>;
 export type Handler = (request: Request, params: Record<string, string>) => RouteResponse;
 export type AppOptions = Omit<Deno.ListenOptions | Deno.ListenTlsOptions, "transport">;
 export type Pattern = RegExp | string;
-export type JSONLike = Record<string, unknown> | Record<string, unknown>[];
+export type JSONLike = Record<string, any> | Record<string, any>[] | any[] | null;
 
 // Optional
 export interface ResponseHelperOptions {
